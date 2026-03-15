50d6dc8462be2c70988f7f111c54b87f33aeb95a
Alan <alan4tomaz8@gmail.com>
2026-03-15 13:10:14 -0300

    Implementado Lógica

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+// IMPLEMENTADO
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456", "Nokia 264", "666666666666666", memoria: 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Discord");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("789325", "Iphone 16", "2222222222222222", memoria: 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
